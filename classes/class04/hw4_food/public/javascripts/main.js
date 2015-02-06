9cc5662e02479ad8ef875aa2744b9990832e9303
Rahil <rahil.dedhia@students.olin.edu>
2015-02-06 18:32:51 -0500

    struggling through async rendering on page for ingredients page

--- classes/class04/hw4_food/public/javascripts/main.js
+++ classes/class04/hw4_food/public/javascripts/main.js
@@ -1,25 +1,58 @@
-var $form = $("#ajax-form");
+var $form = $('#ingr-form');
+var $food = $('.food');
+var $stock = $('.stock');
 
-var onSuccess = function(data, status) {
-  var img = "<img src='"+data+"'/>";
-  $("#result").html(img);
+var onSuccess = function(id, data, status) {
+  console.log(id);
+  console.log(data);
+  $('this').html('Fuck you');
+  // $('#result').html('I so sleepy');
 };
 
+var editSuccess = function(data, status) {
+  console.log(data);
+  $('#result').html('I so sleepy');
+};
+
+var successStock = function(data, status) {
+  console.log(data);
+  $('this').closest('input').html(data);
+}
+
 var onError = function(data, status) {
-  console.log("status", status);
-  console.log("error", data);
+  console.log('status', status);
+  console.log('error', data);
 };
 
 $form.submit(function(event) {
   event.preventDefault();
   formData = $form.serialize();
-  // var mood = $form.find("[name='mood']:checked").val();
-  // var name = $form.find("[name='name']").val();
-  // formData = {
-  //   mood: mood,
-  //   name: name
-  // }
-  $.get("getCat", formData)
+  $.get('getFood', formData)
     .done(onSuccess)
+    .error(onError);
+});
+
+$food.submit(function(event) {
+  var data = {}
+  event.preventDefault();
+
+  id = this.closest('div')['id'];
+  data['id'] = id;
+  data['name'] = prompt('Provide a new food name');
+  data['price'] = prompt('Provide a new price');
+
+  $.get('modify', data)
+    .done(onSuccess(id, data))
+    .error(onError);
+});
+
+$stock.submit(function(event) {
+  var data = {}
+  event.preventDefault();
+
+  data['id'] = this.closest('div')['id'];
+
+  $.get('stock', data)
+    .done(successStock)
     .error(onError);
 });
