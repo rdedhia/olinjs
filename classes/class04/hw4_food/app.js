ef958143ee56095a7db23dfc2ece8d4d8971e369
Rahil <rahil.dedhia@students.olin.edu>
2015-02-06 18:31:50 -0500

    added routing for all the pages

--- classes/class04/hw4_food/app.js
+++ classes/class04/hw4_food/app.js
@@ -1,38 +1,43 @@
-var express = require("express");
-var path = require("path");
-var logger = require("morgan");
-var cookieParser = require("cookie-parser");
-var bodyParser = require("body-parser");
-var exphbs  = require("express-handlebars");
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs  = require('express-handlebars');
 var mongoose = require('mongoose');
 
-var index = require("./routes/index");
-var getCat = require("./routes/getCat");
-
+var index = require('./routes/index');
+var getCat = require('./routes/getCat');
 var schema = require('./models/schema');
+var getFood = require('./routes/getFood');
+
 var Food = schema.Food;
 
 var app = express();
 
 var PORT = process.env.PORT || 3000;
-var mongoURI = process.env.MONGOURI || "mongodb://localhost/test";
+var mongoURI = process.env.MONGOURI || 'mongodb://localhost/test';
 
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", index.home);
+app.get('/', index.home);
 app.get('/ingredients', index.ingredients);
-app.get("/getCat", getCat.getCatGET);
-app.post("/getCat", getCat.getCatPOST);
+app.get('/orders', index.orders);
+app.get('/kitchen', index.kitchen);
+
+app.get('/getFood', getFood.addFood);
+app.get('/modify', getFood.modify);
+app.get('/stock', getFood.stock);
 
 mongoose.connect(mongoURI);
 
 app.listen(PORT, function() {
-  console.log("App running on port:", PORT);
+  console.log('App running on port:', PORT);
 });
--- classes/class04/hw4_food/routes/index.js
+++ classes/class04/hw4_food/routes/index.js
@@ -1,10 +1,57 @@
+var schema = require('./../models/schema.js');
+var Food = schema.Food;
+var Order = schema.Order;
+
 var home = function(req, res){
-  res.render("home");
+  res.render('home');
 };
 
 var ingredients = function(req, res) {
-  res.render("ingredients", {price: 5});
+  // Food.remove({}, function callback() {});
+
+  Food.find({}).exec(function(err, foods) {
+    if (err) {
+      res.send('All the food is fucked');
+    } else {
+      console.log(foods);
+      res.render('ingredients', {ingrs: foods});
+    }
+  });
+}
+
+var orders = function(req, res) {
+  Food.find({}).exec(function(err, foods) {
+    if (err) {
+      res.send('All the food is fucked');
+    } else {
+      console.log(foods);
+      // changing true and false to '' and disabled for checkboxes
+      for (var i=0; i<foods.length; i++) {
+        if (foods[i].stock) {
+          foods[i].enable = '';
+        } else {
+          foods[i].enable = 'disabled'
+        }
+      }
+      console.log(foods);
+      res.render('orders', {ingrs: foods});
+    }
+  });
+}
+
+var kitchen = function(req, res) {
+  Order.find({completed: false})
+    .exec(function(err, orders) {
+      if (err) {
+        res.send('All the orders are fucked');
+      } else {
+        console.log(orders);
+        res.render('kitchen', {ords: orders});
+      }
+    });   
 }
 
 module.exports.home = home;
 module.exports.ingredients = ingredients;
+module.exports.orders = orders;
+module.exports.kitchen = kitchen;
